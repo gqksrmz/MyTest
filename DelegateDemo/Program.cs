17fa3d1f9c27f3484c4d29177647f8ddca56bd2c
1825346742@qq.com <1825346742@qq.com>
2019-11-01 11:31:13 +0800

    Func委托

---
+++ DelegateDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DelegateDemo
+{
+    //逆变只能输入，协变可以输出
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int fac = 10;
+            Func<int, int> tFunc = x => x * fac;
+            Console.WriteLine(tFunc(5));
+
+
+        }
+    }
+
+   
+}
